d1e569100c0dfbd0e9cf2699005294e5f87d4fa3
zhangwei <354190012@qq.com>
2020-07-19 19:13:25 +0800

    增加解析处理

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-let cookie_fun88 = "ASP.NET_SessionId=nf4npligqhpliyra21vnrfs0; FUserSkin=1; SkinMode=3; _Mculture=zh-CN; AppVersion=0; homeUrl=; _ga=GA1.2.2069441260.1595046825; _gid=GA1.2.1192122236.1595046825; .ASPXAUTH=AB11DFA232E7874534EFF445DEF37EFAEC98FB2125B1EAC2AC32B32D43FEB12B0865AB05471EA66588704426CD920AF5822735792C78D5F732A4B82781BEC6EA76623E81646C2E06497247F592F7C252FEBC8A9602E6F907E7E9A52797141FEEA6EF3BE4; _gat=1";
-let cookie_kok = "ASP.NET_SessionId=ecehvenc5x0qjs21tl5nnvwn; redirect=done; sbmwl3-yb4=1813122826.20480.0000; mc=wk12akokdavidchang; opCode=YB5; lobbyUrl=localhost; logoutUrl=localhost; timeZone=480; settingProfile=OddsType=1&NoOfLinePerEvent=1&SortBy=1&AutoRefreshBetslip=False; _ga=GA1.2.1854731626.1595041177; _gid=GA1.2.561131845.1595041177; historyUrl=%2Fm%2Fzh-cn%2Fsports%3Ftheme%3DYB5%26sni%3D4zazfdwjeswmrd5jqfdhiozn%7C%2Fm%2Fzh-cn%2Fsports%2Ffootball%2Fselect-competition%2Fdefault%3Fsc%3DABIBAI%26theme%3DYB5%7C%2Fm%2Fzh-cn%2Fsports%2Ffootball%2Fmatches-by-date%2F2020-07-18%2Ffull-time-asian-handicap-and-over-under%3Fsc%3DABIBAI%26competitionids%3D26326%26theme%3DYB5%7C%2Fm%2Fzh-cn%2Fsports%2Ffootball%2Fselect-competition%2Fdefault%3Fsc%3DABIBAI%26theme%3DYB5";
-
+const fs = require("fs");
+const cookie_fun88 = fs.readFileSync("cookie_fun88.txt").toString();
+const cookie_kok = fs.readFileSync("cookie_kok.txt").toString();
+const userAgent  = "Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3882.0 Mobile Safari/537.36";
 
 const header_fun88 = {
     "Accept": "application/json, text/javascript, */*; q=0.01",
@@ -17,7 +18,7 @@ const header_fun88 = {
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "same-origin",
     "uid": "AB88RMB018268",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+    "User-Agent": userAgent,
     "X-Requested-With": "XMLHttpRequest"
 }
 
@@ -31,7 +32,7 @@ const header_kok = {
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
     "x-requested-with": "XMLHttpRequest",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+    "User-Agent": userAgent,
     "referrer": "https://xj-mbs-yb5.2r9qgy.com/m/zh-cn/sports/football/select-competition/default?sc=ABIBAI&theme=YB5",
     "referrerPolicy": "no-referrer-when-downgrade",
     "cookie": cookie_kok
@@ -40,15 +41,13 @@ const header_kok = {
 
 
 const fun88_data_url = "https://ismart.fafafahuat.com/Odds/ShowAllOdds";
+const fun88_url_getMarket = "https://ismart.fafafahuat.com/Odds/GetMarket";
 
-const fun88_login_url = "";
 
 const kok_data_url = "https://xj-mbs-yb5.2r9qgy.com/zh-cn/Service/CentralService?GetData&ts=1595063492728";
 
-const kok_login_url = "";
-
 
 module.exports = {
-    fun88_data_url, fun88_login_url, kok_data_url, kok_login_url, header_fun88, header_kok
+    fun88_data_url, kok_data_url, header_fun88, header_kok,userAgent,fun88_url_getMarket
 }
 
--- index.js
+++ index.js
@@ -3,23 +3,48 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 const fs = require("fs");
+let cStart = require('child_process');
+const os = require("os")
 const config = require("./config");
 const superagent = require("superagent");
 const cheerio = require("cheerio");
+const Puppeteer = require('puppeteer-core');
 
 
-function doLogin(){
-    console.log("需要登录");
+
+
+function printInfo(ck) {
+    console.log("\n######################################################################\n" +
+        "######################################################################\n" +
+        "########                                                      ########\n" +
+        "########                       程序已" + ck + "                     ########\n" +
+        "########          技术指导电话(微信)：张工 13714608818        ########\n" +
+        "########          技术指导电话(微信)：张工 13714608818        ########\n" +
+        "########          技术指导电话(微信)：张工 13714608818        ########\n" +
+        "########          技术指导电话(微信)：张工 13714608818        ########\n" +
+        "########          技术指导电话(微信)：张工 13714608818        ########\n" +
+        "########                                                      ########\n" +
+        "######################################################################\n" +
+        "######################################################################\n");
+    if (ck == "退出") {
+        process.exit();
+    }
 }
 
-function initData(data){
+let g_data = {};
+
+function initData_fun88(data) {
     // console.log(data);
     data = data.Data;
     let matchList = data.NewMatch;
     let teamNameMap = data.TeamN;
     let leagueNameMap = data.LeagueN;
-    for(var i=0;i<matchList.length;i++){
+    let timestamp = new Date().getTime();
+    let timestamp_diff = 1000 * 60 * 60 * 12;
+    for (var i = 0; i < matchList.length; i++) {
         let match = matchList[i];
+        let matchId = match.MatchId;
+        let matchCode = match.MatchCode;
         let leagueId = match.LeagueId;
         let homeTeamId = match.TeamId1;
         let awayTeamId = match.TeamId2;
@@ -27,15 +52,53 @@ function initData(data){
         let awayName = teamNameMap[awayTeamId].split("|")[0];
         let leagueName = leagueNameMap[leagueId].split("|")[0];
         let gameTime = match.GameTime;
-        let d = new Date(gameTime);
-        let timePart = gameTime.split(/[-T:]/);
-        let d1 = Date.UTC(parseInt(timePart[0]),parseInt(timePart[1]-1),parseInt(timePart[2]),parseInt(timePart[3]),parseInt(timePart[4]),parseInt(timePart[5]),0);
-        console.log(leagueName,homeName,awayName,gameTime,d.toLocaleString(),d1.toLocaleString());
+        let playTime = new Date(new Date(gameTime).getTime() + timestamp_diff);
+        if (playTime < timestamp) {
+
+            continue;
+
+        }
+        let matchObj = { matchId, matchCode, leagueId, homeTeamId, awayTeamId, leagueName, homeName, awayName, gameTime, playTime };
+        // let matchObj = { leagueName, homeName, awayName, playTime,match };
+        if (leagueName.indexOf("虚拟赛") != -1) {
+            continue;
+        }
+        g_data["_" + matchId] = matchObj;
+        // console.log(JSON.stringify(matchObj ));
+        superagent
+            .post(config.fun88_url_getMarket)
+            .set(config.header_fun88)
+            .send({
+                "GameId": "1",
+                "DateType": "t",
+                "BetTypeClass": "OU",
+                "Matchid": matchId
+            })
+            .end((error, response) => {
+                if (error) {
+                    console.log(error)
+                    console.log(leagueName, homeName, awayName, playTime, "抓取失败");
+                    return;
+                }
+                let data = response.body;
+                if (data) {
+                    if (data.ErrorCode == 0) {
+                        let oddsList = data.Data.NewOdds;
+                        let matchId = oddsList[0].MatchId;
+                        g_data["_" + matchId].oddsList = oddsList;
+                    } else if (data.ErrorCode == 100) {
+                        console.log("获取赔率失败 cookie");
+                    }
+                } else {
+                    console.log(response.text);
+                }
+            });
     }
 }
 
-function main(){
 
+function getData_fun88() {
+    console.log("开始抓取fun88的数据");
     superagent
         .post(config.fun88_data_url)
         .set(config.header_fun88)
@@ -52,14 +115,135 @@ function main(){
             let data = response.body;
             if (data) {
                 if (data.ErrorCode == 0) {
-                    initData(data);
+                    initData_fun88(data);
                 } else if (data.ErrorCode == 100) {
-                    doLogin()
+                    // doLogin()
+                    console.log("需要登录，请重新设置 cookie");
                 }
             } else {
                 console.log(response.text);
             }
-        });　　
+        });
+}
+
+function initData_kok(data) {
+    //IsFirstLoad: false
+    // VersionH: 1:0,2:0,3:0,4:0,7:0,13:0,20:0,21:0,23:0,26:0
+    // IsEventMenu: false
+    // SportID: 1
+    // CompetitionID: -1
+    // reqUrl: /m/zh-cn/sports/football/matches-by-date/today/full-time-asian-handicap-and-over-under?competitionids=28485
+    // IsMobile: true
+    // oCompetitionId: 0
+    // oEventIds: 0
+    // oIsFirstLoad: false
+    // oPageNo: 0
+    // oSortBy: 1
+    // LiveCenterEventId: 0
+    // LiveCenterSportId: 1
+    for (var i = 0; i < data.mod.cm.length; i++) {
+        let contry = data.mod.cm[i];//国家
+        let leagueArr = contry.c;//赛事
+        for (var j = 0; j < leagueArr.length; j++) {
+            let league = leagueArr[j];
+            let leagueName = league.n;
+            let leagueId = league.id;
+            superagent
+                .post(config.kok_data_url)
+                .set(config.header_kok)
+                .send({
+                    "IsFirstLoad": "true",
+                    "VersionH": "1:0,2:0,3:0,4:0,7:0,13:0,20:0,21:0,23:0,26:0",
+                    "IsEventMenu": "false",
+                    "SportID": 1,
+                    "CompetitionID": -1,
+                    "reqUrl": "/m/zh-cn/sports/football/select-competition/default?date=today",
+                    "IsMobile": "true",
+                    "oCompetitionId": 0,
+                    "oEventIds": 0,
+                    "oIsFirstLoad": "true",
+                    "oPageNo": 0,
+                    "oSortBy": 1,
+                    "LiveCenterEventId": 0,
+                    "LiveCenterSportId": 1
+                })
+                .end((error, response) => {
+                    if (error) {
+                        console.log(error)
+                        return;
+                    }
+                    // console.log(response.text);
+                    // console.log(response.body);
+                    let data = response.body;
+                    if (data) {
+                        data = data.mod.cm || data.mod.d;
+                        if (data.mod && data.mod.cm && data.mod.cm.length > 0) {
+                            initData_kok(data);
+                        } else {
+                            // doLogin()
+                            console.log("需要登录，请重新设置 cookie");
+                        }
+                    } else {
+                        console.log("获取数据失败 请重新设置 cookie");
+                        console.log(response.text);
+                    }
+                });
+        }
+    }
+
+}
+
+function getData_kok() {
+    console.log("开始抓取kok的数据");
+    superagent
+        .post(config.kok_data_url)
+        .set(config.header_kok)
+        .send({
+            "IsFirstLoad": "true",
+            "VersionH": "1:0,2:0,3:0,4:0,7:0,13:0,20:0,21:0,23:0,26:0",
+            "IsEventMenu": "false",
+            "SportID": 1,
+            "CompetitionID": -1,
+            "reqUrl": "/m/zh-cn/sports/football/select-competition/default?date=today",
+            "IsMobile": "true",
+            "oCompetitionId": 0,
+            "oEventIds": 0,
+            "oIsFirstLoad": "true",
+            "oPageNo": 0,
+            "oSortBy": 1,
+            "LiveCenterEventId": 0,
+            "LiveCenterSportId": 1
+        })
+        .end((error, response) => {
+            if (error) {
+                console.log(error)
+                return;
+            }
+            // console.log(response.text);
+            // console.log(response.body);
+            let data = response.body;
+            if (data) {
+                data = data.mod .cm ;
+                if (data && data.length > 0) {
+                    initData_kok(data);
+                } else {
+                    // doLogin()
+                    console.log("需要登录，请重新设置 cookie");
+                }
+            } else {
+                console.log("获取数据失败 请重新设置 cookie");
+                console.log(response.text);
+            }
+        });
+}
+
+function main() {
+    printInfo("启动");
+
+    // getData_fun88();
+
+    getData_kok();
+
 }
 
 
